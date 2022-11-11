f8e745d150fad9a892cf44284d889f9c6334d3d9
BezukhVladimir <vladimir.bezukh@yandex.ru>
2022-11-11 19:21:25 +0500

    solution to fourth problem was added

--- Lab 1/tasks solution.py
+++ Lab 1/tasks solution.py
@@ -1,3 +1,4 @@
+from math import prod
 from random import randint
 from statistics import median
 
@@ -6,7 +7,7 @@ log = open("log.txt", 'w')
 a = 0
 a_n = 999999
 n = 1000000
-d = 1 # difference of successive members
+d = 1  # difference of successive members
 
 # first task
 series_sum = (a + a_n) * n / 2
@@ -19,7 +20,11 @@ log.write("Среднее значение ряда: " + str(series_mean) + '\n'
 # third task
 random_number_series = []
 for i in range(1000000):
-    random_number_series.append(randint(1, 100))
-log.write("Медиана ряда 1 000 000 целых случайных чисел от 1 до 100: "
+    random_number_series.append(randint(1, 10))
+log.write("Медиана ряда 1 000 000 целых случайных чисел от 1 до 10: "
           + str(median(random_number_series))
           + '\n')
+
+# fourth task
+log.write("Произведение ряда 1 000 000 целых случайных чисел от 1 до 10: "
+          + str(prod(random_number_series)))
