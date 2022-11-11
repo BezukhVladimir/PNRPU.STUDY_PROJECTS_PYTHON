593dcf6533294066f73c48baeda2105ead189982
BezukhVladimir <vladimir.bezukh@yandex.ru>
2022-11-11 18:51:43 +0500

    solution to second problem was added

---
+++ Lab 1/tasks solution.py
@@ -0,0 +1,14 @@
+log = open("log.txt", 'w')
+
+a = 0
+a_n = 999999
+n = 1000000
+d = 1 # difference of successive members
+
+# first task
+series_sum = (a + a_n) * n / 2
+log.write("Сумма ряда 0 - 999 999:\t" + str(series_sum) + '\n')
+
+# second task
+series_mean = series_sum / n
+log.write("Среднее значение ряда:\t" + str(series_mean) + '\n')
