bd7b406c6792b5a595cfa86b6a89b5e107e42225
BezukhVladimir <vladimir.bezukh@yandex.ru>
2022-11-28 18:32:03 +0500

    lab 2 release

--- Lab 2/main.py
+++ Lab 2/main.py
@@ -1,1 +1,18 @@
 import staff
+
+employee = staff.Employee("Vladimir Bezukh", 22, "Student")
+
+# save
+employee.save("employee_data.json")
+print("Saved information:")
+employee.print()
+
+# change
+employee.reposition("Python programmer")
+print("\nChange of position:")
+employee.print()
+
+# load
+employee.load("employee_data.json")
+print("\nLoaded information:")
+employee.print()
--- Lab 2/staff.py
+++ Lab 2/staff.py
@@ -1,18 +1,56 @@
+import json
+
+
 class Person:
-    def __init__(self):
-        self.__fullname = ""
-        self.__age = 0
+
+    def __init__(self, fullname, age):
+        self.fullname = fullname
+        self.age = age
 
     def print(self):
-        print("Fullname:", self.__fullname)
-        print("Age:", self.__age)
+        print("Fullname:", self.fullname)
+        print("Age:", self.age)
+
+    def save(self, json_filepath: str):
+        to_json = {
+            "fullname": self.fullname,
+            "age": self.age,
+        }
+        with open(json_filepath, 'w') as file:
+            json.dump(to_json, file)
+
+    def load(self, json_filepath: str):
+        with open(json_filepath) as file:
+            obj = json.load(file)
+            self.fullname = obj["fullname"]
+            self.age = obj["age"]
 
 
 class Employee(Person):
-    def __init__(self):
-        super().__init__()
-        self.__position = ""
+
+    def __init__(self, fullname, age, position):
+        super().__init__(fullname, age)
+        self.position = position
 
     def print(self):
         super().print()
-        print("Position:", self.__position)
+        print("Position:", self.position)
+
+    def reposition(self, new_position):
+        self.position = new_position
+
+    def save(self, json_filepath: str):
+        to_json = {
+            "fullname": self.fullname,
+            "age": self.age,
+            "position": self.position
+        }
+        with open(json_filepath, 'w') as file:
+            json.dump(to_json, file)
+
+    def load(self, json_filepath: str):
+        with open(json_filepath) as file:
+            obj = json.load(file)
+            self.fullname = obj["fullname"]
+            self.age = obj["age"]
+            self.position = obj["position"]
