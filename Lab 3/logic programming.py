355fefc503ed792252489f122c76577f752f38c0
BezukhVladimir <vladimir.bezukh@yandex.ru>
2022-12-15 21:12:36 +0500

    lab 3, second task

--- Lab 3/logic programming.py
+++ Lab 3/logic programming.py
@@ -10,4 +10,10 @@ pyDatalog.create_terms('series_sum, N, Series_sum')
 
 series_sum[0] = 0
 series_sum[N] = N + series_sum[N - d]
-log.write("Сумма ряда 0 - 999 999: " + str(series_sum[a_n] == Series_sum) + '\n')
+log.write("Сумма ряда 0 - 999\n" + str(series_sum[a_n] == Series_sum) + "\n\n")
+
+# second task
+pyDatalog.create_terms('series_mean, Series_mean')
+
+series_mean[N] = series_sum[N] / N
+log.write("Среднее значение ряда\n" + str(series_mean[999] == Series_mean) + "\n\n")
